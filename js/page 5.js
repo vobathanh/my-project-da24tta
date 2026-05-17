64745a19b8d2c03640d81e28188670891ffbb0a7
vobathanh <vobathanh2006@gmail.com>
2026-05-17 22:38:44 +0700

    update

--- js/page 5.js
+++ js/page 5.js
@@ -3,50 +3,75 @@ const productList = [
         id: 1,
         name: "Hoa hồng",
         price: "200000",
-        image: "../asset/images/hoa.jpg",
-        productLink:  "https://giaohoatannoi247.com/shop-hoa-tuoi-tra-vinh.html?srsltid=AfmBOooKtxbWh8eILx3T5JI4U8mGMC4Yby8pN_FfObOoKFmFw55mfh9n"
+        image: "../asset/image/hoa.jpg",
+        productLink: "https://giaohoatannoi247.com/shop-hoa-tuoi-tra-vinh.html"
     },
     {
         id: 2,
-        name: "Hoa Tulip ",
-        price: "550000 VND ",
-        image: "../asset/images/tulip.jpg",
+        name: "Hoa Tulip",
+        price: "550000",
+        image: "../asset/image/tulip.jpg",
         productLink: "https://giaohoatannoi247.com/bo-hoa-tulip.html"
     },
-
     {
         id: 3,
-        name: "Hoa hướng dương ",
-        price: "250000 VND ",
-        image: "../asset/images/huongduong.jpg",
+        name: "Hoa hướng dương",
+        price: "250000",
+        image: "../asset/image/huongduong.jpg",
         productLink: "https://giaohoatannoi247.com/bo-huong-duong-dep.html"
     },
-
-   
-    
+    {
+        id: 4,
+        name: "Hoa lục bình",
+        price: "300000",
+        image: "../asset/image/lucbinh.jpg",
+        productLink: "https://giaohoatannoi247.com/bo-huong-duong-dep.html"
+    },
+    {
+        id: 5,
+        name: "Hoa ly",
+        price: "250000",
+        image: "../asset/image/hoaly.jpg",
+        productLink: "https://giaohoatannoi247.com/bo-huong-duong-dep.html"
+    },
+    {
+        id: 6,
+        name: "Hoa ly vàng",
+        price: "250000",
+        image: "../asset/image/hoalyvang.jpg",
+        productLink: "https://giaohoatannoi247.com/bo-huong-duong-dep.html"
+    },
+    {
+        id: 7,
+        name: "Hoa cẩm tú cầu",
+        price: "250000",
+        image: "../asset/image/camtucau.jpg",
+        productLink: "https://giaohoatannoi247.com/bo-huong-duong-dep.html"
+    },
+    {
+        id: 8,
+        name: "Hoa sen",
+        price: "250000",
+        image: "../asset/image/hoasen.jpg",
+        productLink: "https://giaohoatannoi247.com/bo-huong-duong-dep.html"
+    }
 ];
 
-function addProduct(id, name, price, image, link)
-{
-    //Khai báo item
+function addProduct(id, name, price, image, link) {
     const productItem = document.createElement("div");
-    productItem.setAttribute("class", "product-item col-md-2");
+    productItem.className = "product-item";
 
-    //Div hình
     const productDiv1 = document.createElement("div");
-    productDiv1.setAttribute("class", "product-image");
+    productDiv1.className = "product-image";
 
-    //Tạo hình
     const productImage = document.createElement("img");
-    productImage.setAttribute("src", image);
-    productImage.setAttribute("alt", name);
-    productImage.setAttribute("class", "img-thumbnail img-fluid");
+    productImage.src = image;
+    productImage.alt = name;
 
-    //Gán hình
     productDiv1.appendChild(productImage);
 
     const productDiv2 = document.createElement("div");
-    productDiv2.setAttribute("class", "product-info");
+    productDiv2.className = "product-info";
 
     const productName = document.createElement("p");
     productName.className = "title";
@@ -58,30 +83,20 @@ function addProduct(id, name, price, image, link)
 
     const productLink = document.createElement("a");
     productLink.innerText = "Xem chi tiết";
+    productLink.href = link;
+    productLink.target = "_blank";
 
-    productLink.setAttribute("href", link);
-    productLink.setAttribute("target", "_blank");
-
-
-    //Gán vào div2
     productDiv2.appendChild(productName);
     productDiv2.appendChild(productPrice);
     productDiv2.appendChild(productLink);
 
-
     productItem.appendChild(productDiv1);
     productItem.appendChild(productDiv2);
 
-    //Gán vào danh sách
-    const productContainer = document.getElementById("product-list");
-
-    productContainer.appendChild(productItem);
+    document.getElementById("product-list").appendChild(productItem);
 }
 
-
-//Render sản phẩm
-productList.forEach(function(product) {
-
+productList.forEach(product => {
     addProduct(
         product.id,
         product.name,
@@ -89,5 +104,4 @@ productList.forEach(function(product) {
         product.image,
         product.productLink
     );
-
 });
